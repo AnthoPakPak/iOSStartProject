bc02ebb98f454b5866104cdb86b3fee31c62c877
Anthonin Cocagne <acocagne@servicesnomades.com>
2018-07-16 14:48:03 +0200

    Add layoutIfNeeded in UIView category for rounded corners

--- StartProject/Miscellaneous/Categories/UIView+Additions.m
+++ StartProject/Miscellaneous/Categories/UIView+Additions.m
@@ -10,7 +10,10 @@
 
 @implementation UIView (Additions)
 
+// Can cause layout bugs if call before autolayout is finished. May be called in viewDidLayoutSubviews
 -(void)roundCornersOnTopLeft:(BOOL)tl topRight:(BOOL)tr bottomLeft:(BOOL)bl bottomRight:(BOOL)br radius:(float)radius {
+    [self layoutIfNeeded]; // Without this, we can sometimes get wrong frame size
+
     if (tl || tr || bl || br) {
         UIRectCorner corner = 0;
         if (tl) {corner = corner | UIRectCornerTopLeft;}
