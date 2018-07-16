af43e25f7bb2a6eea6d86fe6e47baa5066a3c0ee
Anthonin Cocagne <acocagne@servicesnomades.com>
2018-07-16 15:06:16 +0200

    layoutIfNeeded causes wrong layout in some cases (UITableViewCell for example), so rollback

--- StartProject/Miscellaneous/Categories/UIView+Additions.m
+++ StartProject/Miscellaneous/Categories/UIView+Additions.m
@@ -10,9 +10,9 @@
 
 @implementation UIView (Additions)
 
-// Can cause layout bugs if call before autolayout is finished. May be called in viewDidLayoutSubviews
+// Can cause layout bugs if call before autolayout is finished. May be called in viewDidLayoutSubviews, or call [self layoutIfNeeded] just before
 -(void)roundCornersOnTopLeft:(BOOL)tl topRight:(BOOL)tr bottomLeft:(BOOL)bl bottomRight:(BOOL)br radius:(float)radius {
-    [self layoutIfNeeded]; // Without this, we can sometimes get wrong frame size
+//    [self layoutIfNeeded]; // Without this, we can sometimes get wrong frame size
 
     if (tl || tr || bl || br) {
         UIRectCorner corner = 0;
